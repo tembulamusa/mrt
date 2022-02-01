190cf76c956e16370af89c0560161559d62b1d3d
kelvinkibugi@gmail.com <kelvinkibugi@gmail.com>
2022-02-01 13:06:26 +0300

    Added footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,23 +1,79 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import {faFacebook, faInstagram, faTwitter, faYoutube} from "@fortawesome/free-brands-svg-icons"
+
 
 const Footer = (props) => {
 
     return (
-        <footer className="">                                                               
-            <div className="footer-upper">                                                  
-                <a href="/terms" style={{paddingLeft:0}} 
-                    title="Terms">Terms and Conditions</a> | 
-                    <a href="#"> 18+ </a> | 
-                    <a href="#" title="Highlights"> Highlights </a> | 
-                    <a href="#" title="Football"> FOOTBALL: Todays Games</a>  | 
-                   <a href="#">Upcoming</a>
-            </div>                                                                      
-            <div className="container" id="navbar-collapse-main">                               
-               <div className="footer-bottom">                                                 
-                                                                                        
-                                                                                        
-                </div>                                                                      
-            </div>                                                                      
-        </footer>  
+        <footer className="footer-custom">
+            <div className="col-md-12 d-flex">
+                <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3 text-white">
+                    <h5>BETNARE</h5>
+                    <ul>
+                        <li className="">
+                            P.O.BOX 00100 NAIROBI KENYA
+                        </li>
+                        <li className="">
+                            <a href="mailto:care@ke.betnare.com">
+                                <FontAwesomeIcon icon={faEnvelope}/> info@betnare.com
+                            </a>
+                        </li>
+                        <li className="footer-icon">
+                            <a href="https://www.facebook.com/BetnareKenya">
+                                <FontAwesomeIcon icon={faFacebook}/> Facebook
+                            </a>
+                        </li>
+                        <li className="footer-icon">
+                            <a href="https://twitter.com/betnare">
+                                <FontAwesomeIcon icon={faTwitter}/> Twitter
+                            </a>
+                        </li>
+                        <li className="footer-icon">
+                            <a href="https://www.youtube.com/user/betnare">
+                                <FontAwesomeIcon icon={faYoutube}/> YouTube
+                            </a>
+                        </li>
+                        <li className="footer-icon">
+                            <a href="https://instagram.com/betnare">
+                                <FontAwesomeIcon icon={faInstagram}/> Instagram
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <h5>TERMS AND CONDITIONS</h5>
+                    <ul>
+                        <li className="">
+                            P.O.BOX 25675-00100 NAIROBI
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <h5>LEGAL</h5>
+                    <ul>
+                        <li className="text-danger">
+                            You must be 18 Years+ to use this website.
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                    <h5>LICENSING</h5>
+                    <hr/>
+                    <p>
+                        We are licensed and regulated by the Betting Control and Licensing
+                        Board.
+                    </p>
+                </div>
+            </div>
+            <div className="container" id="navbar-collapse-main">
+                <div className="footer-bottom text-center">
+                    Copyright &copy; {new Date().getFullYear()} All rights Reserved.
+                </div>
+            </div>
+        </footer>
     )
 }
+
+export default Footer
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-import React,  { useContext, useEffect, useState } from "react";
-import { useLocation } from 'react-router-dom';
+import React, {useContext, useEffect, useState} from "react";
+import {useLocation} from 'react-router-dom';
 
 import Header from './header/header';
 import Footer from './footer/footer';
@@ -11,58 +11,59 @@ import SearchBar from './header/search-bar';
 import MatchList from './matches/index';
 import Right from './right/index';
 
-import { getBetslip } from './utils/betslip' ;
+import {getBetslip} from './utils/betslip' ;
 
 import useAxios from "../hooks/axios.hook";
-import { Context }  from '../context/store';
+import {Context} from '../context/store';
 
 const Index = (props) => {
     const [page, setPage] = useState(1);
-    const [state, dispatch] = useContext(Context);                              
+    const [state, dispatch] = useContext(Context);
     const {response, makeRequest} = useAxios()
     const [tab, setTab] = useState('highlights');
     const location = useLocation();
 
-    useEffect(()=>{                                                             
-        const abortController = new AbortController();                          
-        let tab =  location.pathname.replace("/","") || 'highlights'; 
-        let endpoint = "/v1/matches?page="+ (state?.page|| 1) +"&tab="+tab;     
-                                                                                
-        makeRequest({url:endpoint, method:"get", data:null }).then((response) => {
-            let {status, result} = response;                      
-            dispatch({type:"SET", key:"matches", payload:result});
-        });                                                                     
-                                                                                
-        return () => {                                                          
-            abortController.abort();                                            
-        };                                                                      
+    useEffect(() => {
+        const abortController = new AbortController();
+        let tab = location.pathname.replace("/", "") || 'highlights';
+        let endpoint = "/v1/matches?page=" + (state?.page || 1) + "&tab=" + tab;
+
+        makeRequest({url: endpoint, method: "get", data: null}).then((response) => {
+            let {status, result} = response;
+            dispatch({type: "SET", key: "matches", payload: result});
+        });
+
+        return () => {
+            abortController.abort();
+        };
     }, [state?.page]);
 
     useEffect(() => {
         let betslip = getBetslip();
-        if(betslip){
-            dispatch({type:"SET", key:"betslip", payload:betslip});
+        if (betslip) {
+            dispatch({type: "SET", key: "betslip", payload: betslip});
         }
     }, []);
 
-   return (
-       <>
-        <Header />        
-        <div className="by amt">
-          <div className="gc">
-            <SideBar />
-            <div className="gz home">
-                <div className="homepage">
-                    <CarouselLoader />
-                    <MainTabs tab={ location.pathname.replace("/","")}/>
-                    <MatchList />
-                </div> 
-            </div>  
-            <Right />
-          </div>
-        </div>
-       </>
-   )
+    return (
+        <>
+            <Header/>
+            <div className="by amt">
+                <div className="gc">
+                    <SideBar/>
+                    <div className="gz home">
+                        <div className="homepage">
+                            <CarouselLoader/>
+                            <MainTabs tab={location.pathname.replace("/", "")}/>
+                            <MatchList/>
+                        </div>
+                    </div>
+                    <Right/>
+                </div>
+            </div>
+            <Footer/>
+        </>
+    )
 }
 
 export default Index
