ce0ddf27ef1f9f54500e781e7dbef673a9cfb813
Reuben Wafula <rubewafula@gmail.com>
2022-03-08 21:48:32 +0300

    FIX: added logic to signup page

---
+++ src/components/pages/signup.js
@@ -0,0 +1,163 @@
+import React, {useState, useEffect, } from 'react';
+import {Formik, Field, Form} from 'formik';
+import makeRequest from "../utils/fetch-request";
+import mpesa from '../../assets/img/mpesa-3.png'
+
+const Header = React.lazy(()=>import('../header/header'));
+const SideBar = React.lazy(()=>import('../sidebar/sidebar'));
+const Right = React.lazy(()=>import('../right/index'));
+const Footer = React.lazy(()=>import('../footer/footer'));
+
+const Signup = (props) => {
+    console.log("Props are ", props)
+
+    const [success, setSuccess] = useState(false);
+    const [message, setMessage] = useState(null);
+
+    const [isLoading, setIsLoading] = useState(false);
+    const initialValues = {
+        msisdn: '',
+        password: ''
+    }
+
+    const handleSubmit = values => {
+        console.log("Form Data posting to api", values)
+        let endpoint = '/v1/signup';
+        makeRequest({url: endpoint, method: 'POST', data: values}).then(([status, response]) => {
+            setSuccess(status === 200 || status === 201);
+            setMessage(response.message);
+			console.log("This are your values ", response,  status);
+        })
+    }
+
+    const validate = values => {
+
+        let errors = {}
+
+        if (!values.msisdn || !values.msisdn.match(/(254|0|)?[71]\d{8}/g) ) {
+            errors.msisdn = 'Please enter a valid phone number'
+        }
+
+        if (!values.password || values.password.length < 4) {
+            errors.password = "Please enter four or more characters for password";
+        }
+
+        return errors
+    }
+
+    const FormTitle = () => {
+       return (
+        <div className='col-md-12 primary-bg p-4 text-center'>
+            <h4 className="inline-block">
+                SIGNUP | CREATE A NEW ACCOUNT
+            </h4>
+        </div>
+       )
+    }
+
+    const MySignupForm = (props) => {
+        const {isValid, errors, values, submitForm, setFieldValue } = props;
+
+        const onFieldChanged = (ev)=>{
+            let field = ev.target.name;
+            let value = ev.target.value;
+            setFieldValue(field, value);
+        }
+        return (
+            <Form > 
+              <div className="pt-0">
+                <div className="row">
+                    <div className='col-md-6 text-center'>
+                        <img src={mpesa} alt=""/>
+                    </div>
+                    <hr/>
+                    <div className="form-group row d-flex justify-content-center mt-5">
+                        <div className="col-md-12">
+                            <label>Mobile Number</label>
+                            <input
+                                value={values.msisdn}
+                                className="text-dark deposit-input form-control col-md-12 input-field"
+                                id="msisdn"
+                                name="msisdn"
+                                type="text"
+                                placeholder='Phone number'
+                                onChange={ev => onFieldChanged(ev)}
+                            />
+                            {errors.msisdn && <div className='text-danger'> {errors.msisdn} </div> }
+                        </div>
+                    </div>
+
+                    <div className="form-group row d-flex justify-content-center mt-5">
+                        <div className="col-md-12">
+                            <label>Password</label>
+                            <input
+                                value={values.password}
+                                className="text-dark deposit-input form-control col-md-12 input-field"
+                                id="password"
+                                name="password"
+                                type="password"
+                                placeholder='Password'
+                                onChange={ev => onFieldChanged(ev)}
+                            />
+                            {errors.password && <div className='text-danger'> {errors.password} </div> }
+                        </div>
+                    </div>
+                    <div className="form-group row d-flex justify-content-left mb-4">
+                        <div className="col-md-3">
+                            <button type="submit" 
+                                onClick={submitForm}
+                                className='btn btn-lg btn-primary mt-5 col-md-12 deposit-withdraw-button'>
+                                Signup
+                            </button>
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </Form>
+        );
+    }
+
+    const SignupForm = (props) => {
+        return (
+             <Formik
+                initialValues={initialValues}
+                onSubmit={handleSubmit}
+                validateOnChange={false}
+                validateOnBlur={false}
+                validate={validate}
+                render = {(props) => <MySignupForm {...props} /> } />
+            );
+    }
+
+    const Alert = (props) => {
+        let c = success ? 'success' : 'danger';
+        return (<div role="alert" className={`fade alert alert-${c} show`}>{message}</div>);
+
+    };
+
+    return (
+            <React.Fragment>
+            <Header/>
+              <div className="by amt">
+                <div className="gc">
+                    <SideBar loadCompetitions />
+                    <div className="gz home">
+                       <div className="homepage">
+                         <FormTitle />
+                           <div className="col-md-12 mt-2 text-white p-2">
+                             { message && <Alert /> }
+                               <div className="modal-body pb-0" data-backdrop="static">
+                                    <SignupForm />
+                               </div>
+                           </div>
+                       </div>
+                    </div>
+                    <Right/>
+                </div>
+              </div>
+              <Footer/>
+            </React.Fragment>
+        );
+}
+
+export default Signup;
