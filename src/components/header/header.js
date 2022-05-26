ad64308bee61e7e91c08940e6595cdc51a1149e9
Waithira <kelvinkibugi@gmail.com>
2022-05-26 17:40:45 +0300

    Added sidebar code for mobile devices

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,66 +1,99 @@
-import React, { useEffect, useCallback, useState, useContext, useRef} from 'react';
-import { useNavigate } from "react-router-dom"
+import React, {useEffect, useCallback, useState, useContext, useRef} from 'react';
+import {useNavigate} from "react-router-dom"
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import { Context }  from '../../context/store';
-import { getFromLocalStorage } from '../utils/local-storage';
-import { ToastContainer} from 'react-toastify';
+import {LazyLoadImage} from 'react-lazy-load-image-component';
+import {Context} from '../../context/store';
+import {getFromLocalStorage} from '../utils/local-storage';
+import {ToastContainer} from 'react-toastify';
 import makeRequest from '../utils/fetch-request';
-import { setLocalStorage } from '../utils/local-storage';
+import {setLocalStorage} from '../utils/local-storage';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 import logo from '../../assets/img/logo.png';
-const ProfileMenu = React.lazy(()=>import('./profile-menu'));
-const HeaderLogin = React.lazy(()=>import('./top-login'));
-const HeaderNav = React.lazy(()=>import('./header-nav'));
+import {Navbar, Nav, Offcanvas} from "react-bootstrap";
+import SideBar from "../sidebar/sidebar";
+
+const ProfileMenu = React.lazy(() => import('./profile-menu'));
+const HeaderLogin = React.lazy(() => import('./top-login'));
+const HeaderNav = React.lazy(() => import('./header-nav'));
 
 const Header = (props) => {
     const [user, setUser] = useState(getFromLocalStorage("user"));
     const [, dispatch] = useContext(Context);
     const history = useNavigate();
     const containerRef = useRef();
-    const { current } = containerRef;
+    const {current} = containerRef;
+    const [competitions, setCompetitions] = useState({});
+
+    const fetchData = useCallback(async() => {
+        let cached_categories = getFromLocalStorage('categories');
+        let endpoint = "/v1/categories";
+
+        if(!cached_categories) {
+            const [competition_result] =  await Promise.all([
+                makeRequest({url:endpoint, method:"get", data:null }),
+            ]);
+            let [c_status, c_result] = competition_result
+
+            if(c_status === 200){
+                setCompetitions(c_result);
+            }
+            setLocalStorage('categories', c_result);
+        } else {
+            setCompetitions(cached_categories);
+        }
+
+    }, []);
+
+    useEffect(() => {
+
+        const abortController = new AbortController();
+        fetchData();
+
+        return () => {
+            abortController.abort();
+        };
+    }, [fetchData]);
 
 
     const NotifyToastContaner = () => {
-       return <ToastContainer
-                   position="top-right"
-                   autoClose={5000}
-                   hideProgressBar={false}
-                   newestOnTop={false}
-                   closeOnClick
-                   rtl={false}
-                   pauseOnFocusLoss
-                   draggable
-                   pauseOnHover
-                   />
+        return <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+        />
     };
     const updateUserOnHistory = useCallback(() => {
-        if(!user){
+        if (!user) {
             return false;
         }
-        let endpoint = "/v1/balance";     
+        let endpoint = "/v1/balance";
         let udata = {
-            token:user.token
+            token: user.token
         }
-        makeRequest({url:endpoint, method:"post", data:udata }).then(([_status, response]) => {
-            if(_status == 200){
-                let u = { ...user, ...response.user };
+        makeRequest({url: endpoint, method: "post", data: udata}).then(([_status, response]) => {
+            if (_status == 200) {
+                let u = {...user, ...response.user};
                 setLocalStorage('user', u);
                 setUser(u)
-                dispatch({type:"SET", key:"user", payload:user});
+                dispatch({type: "SET", key: "user", payload: user});
             }
         });
 
     }, [current]);
 
     const updateUserOnLogin = useCallback(() => {
-        dispatch({type:"SET", key:"user", payload:user});
+        dispatch({type: "SET", key: "user", payload: user});
     }, [user?.msisdn, user?.balance]);
 
 
-    
     useEffect(() => {
         updateUserOnHistory()
     }, [updateUserOnHistory])
@@ -70,25 +103,50 @@ const Header = (props) => {
         updateUserOnLogin()
     }, [updateUserOnLogin])
 
+    const expand = "sm"
     return (
-       <Container className="shrink-header" id="shrink-header">
-            <NotifyToastContaner />
-            <Row className="ck pc os app-navbar top-nav">
-                <div className=" col-3">
-                  <div>
-                      <a className="e logo" href="/" title="Betnare">
-                      <LazyLoadImage src={logo} alt="Betnare" title="Betnare" effects ="blur"/>
-                      </a>
-                  </div>
-                </div>
-                <div className="col-9" id="navbar-collapse-main">
-                    { user ?  <ProfileMenu  user={user}/> : <HeaderLogin setUser = {setUser}/> }
-                </div>
-            </Row>
-            <Row className="second-nav ck pc os app-navbar ">
-              <HeaderNav />
-            </Row>
-       </Container>
+        <>
+            <Navbar expand="sm" className="mb-3 ck pc os app-navbar top-nav" fixed="top" variant="dark">
+                <Container fluid>
+                    <Navbar.Brand href="/" className="e logo" title="Betnare">
+                        <div className="col-3">
+                            <div>
+                                <LazyLoadImage src={logo} alt="Betnare" title="Betnare" effects="blur"/>
+                            </div>
+                        </div>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`}/>
+                    <div className="col-9" id="navbar-collapse-main">
+                        {user ? <ProfileMenu user={user}/> : <HeaderLogin setUser={setUser}/>}
+                    </div>
+                    <Row className="second-nav ck pc os app-navbar app-header-nav">
+                        <HeaderNav/>
+                    </Row>
+                    <Navbar.Offcanvas
+                        className='off-canvas second-nav'
+                        id={`offcanvasNavbar-expand-${expand}`}
+                        aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                        placement="start"
+                    >
+                        <Offcanvas.Header closeButton className='text-white'>
+                            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                <div className="col-3">
+                                    <div>
+                                        <LazyLoadImage src={logo} alt="Betnare" title="Betnare" effects="blur"/>
+                                    </div>
+                                </div>
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-end flex-grow-1 p-2">
+                                <SideBar competitions={competitions} override_display={true}/>
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
+        </>
+
     )
 }
 export default React.memo(Header);
