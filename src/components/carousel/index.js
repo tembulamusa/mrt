124fb8eefdc3b42c5287c976921617ef2ca26c18
Waithira <kelvinkibugi@gmail.com>
2022-06-14 19:07:06 +0300

    Added homepage sliders

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -1,16 +1,22 @@
 import React, {useState} from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import {LazyLoadImage} from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
+// import banner1  from '../../assets/img/banner/banner001.jpeg';
 
-import banner1  from '../../assets/img/banner/banner001.jpeg';
-import banner2 from '../../assets/img/banner/win-bonus.jpg';
-import banner3 from '../../assets/img/banner/banner002.png';
-import  banner4 from '../../assets/img/banner/boobsafi.jpg';
-import banner5 from '../../assets//img/banner/lottery-02.jpg';
-import banner6 from '../../assets/img/banner/banner003.jpg';
-import banner7 from '../../assets/img/banner/phase_1_09.jpg';
+import banner1 from '../../assets/img/banner/homepage/KARIBU.png';
+import banner6 from '../../assets/img/banner/homepage/REGISTER.png';
+import banner2 from '../../assets/img/banner/homepage/50Bob.png';
+import banner3 from '../../assets/img/banner/homepage/CASHBACK.png';
+import banner4 from '../../assets/img/banner/homepage/DEPOSIT BONUS.png';
+import banner5 from '../../assets//img/banner/homepage/JACKPOT.png';
+import banner7 from '../../assets/img/banner/homepage/MASTER.png';
+import banner8 from '../../assets/img/banner/homepage/ODDS.png';
+
+const banners = [
+    banner1, banner2, banner3, banner4, banner5, banner6, banner7, banner8
+]
 
 const CarouselLoader = (props) => {
     const [imageLoaded, setImageLoaded] = useState(false);
@@ -20,19 +26,19 @@ const CarouselLoader = (props) => {
 
     return (
         <Carousel>
-         { [banner1].map((banner, idx) => (
-              <Carousel.Item key={idx}>
-                <LazyLoadImage
-                  className="d-block w-100"
-                  style={{display: imageLoaded? 'block': 'none'}}
-                  src={banner}
-                  onLoad={onImageLoaded}
-                  alt="Batnare"
-                  effects="blur"
-                />
-              </Carousel.Item>
-           ))
-         }
+            {banners.map((banner, idx) => (
+                <Carousel.Item key={idx}>
+                    <LazyLoadImage
+                        className="d-block w-100"
+                        style={{display: imageLoaded ? 'block' : 'none'}}
+                        src={banner}
+                        onLoad={onImageLoaded}
+                        alt="Batnare"
+                        effects="blur"
+                    />
+                </Carousel.Item>
+            ))
+            }
 
         </Carousel>
     )
