62bdb884e122c53c075424e22a38cea0c6e5c7ea
Kelvin <kelvin@Kelvins-MacBook-Pro.local>
2022-06-15 20:48:25 +0300

    Removing unused imports

--- src/components/pages/app/index.js
+++ src/components/pages/app/index.js
@@ -1,11 +1,4 @@
 import React from "react";
-import {
-    Accordion,
-    AccordionItem,
-    AccordionItemHeading,
-    AccordionItemButton,
-    AccordionItemPanel,
-} from 'react-accessible-accordion';
 import 'react-accessible-accordion/dist/fancy-example.css';
 
 const Header = React.lazy(() => import('../../header/header'));
