d14e16f7bf81cab3409dd11e424299abb2c5a1f0
Kelvin <kelvin@Kelvins-MacBook-Pro.local>
2022-06-16 14:19:07 +0300

    Updating minimum deposit amount

--- src/components/pages/deposit-withraw/Deposit.js
+++ src/components/pages/deposit-withraw/Deposit.js
@@ -1,19 +1,19 @@
 import React, {useState, useContext, useEffect} from 'react';
 
-import {Formik,  Form} from 'formik';
+import {Formik, Form} from 'formik';
 import makeRequest from "../../utils/fetch-request";
 import mpesa from '../../../assets/img/mpesa.png'
-import { Context }  from '../../../context/store';
+import {Context} from '../../../context/store';
 import {getBetslip} from '../../utils/betslip'
 
-const Header = React.lazy(()=>import('../../header/header'));
-const Footer = React.lazy(()=>import('../../footer/footer'));
-const SideBar = React.lazy(()=>import('../../sidebar/sidebar'));
-const Right = React.lazy(()=>import('../../right/index'));
+const Header = React.lazy(() => import('../../header/header'));
+const Footer = React.lazy(() => import('../../footer/footer'));
+const SideBar = React.lazy(() => import('../../sidebar/sidebar'));
+const Right = React.lazy(() => import('../../right/index'));
 
 
 const Deposit = (props) => {
-    
+
     const [state, dispatch] = useContext(Context);
     const [success, setSuccess] = useState(false);
     const [message, setMessage] = useState(null);
@@ -35,11 +35,11 @@ const Deposit = (props) => {
 
         let errors = {}
 
-        if (!values.msisdn || !values.msisdn.match(/(254|0|)?[71]\d{8}/g) ) {
+        if (!values.msisdn || !values.msisdn.match(/(254|0|)?[71]\d{8}/g)) {
             errors.msisdn = 'Please enter a valid phone number'
         }
 
-        if (!values.amount || values.amount < 50 || values.amount > 70000) {
+        if (!values.amount || values.amount < 1 || values.amount > 70000) {
             errors.amount = "Please enter amount between KES 50 and KES 70, 000";
         }
         return errors
@@ -53,146 +53,156 @@ const Deposit = (props) => {
     }, [])
 
     const FormTitle = () => {
-       return (
+        return (
             <div className='col-md-12 primary-bg p-4 text-center'>
                 <h4 className="inline-block">
                     DEPOSIT FUNDS (MOBILE MONEY)
                 </h4>
             </div>
-       )
+        )
     }
 
 
     const DepositFormFields = (props) => {
-        const { values, errors, onFieldChanged } = props;
-    
+        const {values, errors, onFieldChanged} = props;
+
         return (
             <>
-            <div className="form-group row d-flex justify-content-center">
-                <div className="col-md-12">
-                    <label>Phone Number</label>
-                    <input
-                        readOnly={true}
-                        className="text-dark deposit-input form-control input-field"
-                        id="msisdn"
-                        name="msisdn"
-                        type="text"
-                        value ={values.msisdn}
-                        placeholder='Enter Phone Number'
-                    />
-                    {errors.msisdn &&  <div className='text-danger'> {errors.msisdn} </div>  }
+                <div className="form-group row d-flex justify-content-center">
+                    <div className="col-md-12">
+                        <label>Phone Number</label>
+                        <input
+                            readOnly={true}
+                            className="text-dark deposit-input form-control input-field"
+                            id="msisdn"
+                            name="msisdn"
+                            type="text"
+                            value={values.msisdn}
+                            placeholder='Enter Phone Number'
+                        />
+                        {errors.msisdn && <div className='text-danger'> {errors.msisdn} </div>}
+                    </div>
                 </div>
-            </div>
-            <div className="form-group row d-flex justify-content-center mt-5">
-                <div className="col-md-12">
-                    <label>Amount to Deposit</label>
-                    <input
-                        onChange={ev => onFieldChanged(ev) }
-                        className="text-dark deposit-input form-control col-md-12 input-field"
-                        id="amount"
-                        name="amount"
-                        type="text"
-                        value ={values.amount}
-                        placeholder='Enter Amount'
-                    />
-                    {errors.amount &&  <div className='text-danger'> {errors.amount} </div>  }
+                <div className="form-group row d-flex justify-content-center mt-5">
+                    <div className="col-md-12">
+                        <label>Amount to Deposit</label>
+                        <input
+                            onChange={ev => onFieldChanged(ev)}
+                            className="text-dark deposit-input form-control col-md-12 input-field"
+                            id="amount"
+                            name="amount"
+                            type="text"
+                            value={values.amount}
+                            placeholder='Enter Amount'
+                        />
+                        {errors.amount && <div className='text-danger'> {errors.amount} </div>}
+                    </div>
                 </div>
-            </div>
-            <div className="form-group row d-flex justify-content-left mb-4">
-                <div className="col-md-3">
-                    <button
-                        className='btn btn-lg btn-primary mt-5 col-md-12 deposit-withdraw-button'>
-                        Deposit
-                    </button>
+                <div className="form-group row d-flex justify-content-left mb-4">
+                    <div className="col-md-3">
+                        <button
+                            className='btn btn-lg btn-primary mt-5 col-md-12 deposit-withdraw-button'>
+                            Deposit
+                        </button>
+                    </div>
                 </div>
-            </div>
-         </>
-       )
+            </>
+        )
     }
 
 
     const PaymentInstructions = (props) => {
-         return (
-             <>
+        return (
+            <>
                 <label className='text-info'>Deposit Instructions</label>
                 <div className="container">
-                    <div className="row"><div className="col"> 1. Enter the amount you want to deposit.</div></div>
-                    <div className="row"><div className="col"> 2. Click on the deposit button.</div></div>
-                    <div className="row"><div className="col"> 3. Check your phone for an M-Pesa Request.</div></div>
-                    <div className="row"><div className="col"> 4. Enter your M-Pesa Pin to confirm the transaction.</div></div>
-                    <div className="row"><div className="col"> 5. On successful payment, you will receive an M-Pesa
-                        Confirmation.
-                    </div></div>
+                    <div className="row">
+                        <div className="col"> 1. Enter the amount you want to deposit.</div>
+                    </div>
+                    <div className="row">
+                        <div className="col"> 2. Click on the deposit button.</div>
+                    </div>
+                    <div className="row">
+                        <div className="col"> 3. Check your phone for an M-Pesa Request.</div>
+                    </div>
+                    <div className="row">
+                        <div className="col"> 4. Enter your M-Pesa Pin to confirm the transaction.</div>
+                    </div>
+                    <div className="row">
+                        <div className="col"> 5. On successful payment, you will receive an M-Pesa
+                            Confirmation.
+                        </div>
+                    </div>
                 </div>
             </>
         );
     }
     const MyDepositForm = (props) => {
-        const {errors, values,  setFieldValue } = props;
+        const {errors, values, setFieldValue} = props;
 
-        const onFieldChanged = (ev)=>{
+        const onFieldChanged = (ev) => {
             let field = ev.target.name;
             let value = ev.target.value;
             setFieldValue(field, value);
         }
 
-       return (
-            <Form className="shadow-sm rounded border-0" >
+        return (
+            <Form className="shadow-sm rounded border-0">
                 <div className="pt-0">
                     <div className="row">
                         <div className='col-md-7 text-center'>
                             <img src={mpesa} alt=""/>
                         </div>
                         <hr/>
-                        <DepositFormFields  onFieldChanged ={ onFieldChanged} values ={values } errors={errors} />
+                        <DepositFormFields onFieldChanged={onFieldChanged} values={values} errors={errors}/>
                         <hr/>
-                        <PaymentInstructions />
+                        <PaymentInstructions/>
                     </div>
                 </div>
             </Form>
-        ); 
+        );
     }
 
     const DepositForm = (props) => {
         return (
-             <Formik
+            <Formik
                 initialValues={initialValues}
                 onSubmit={handleSubmit}
                 validateOnChange={false}
                 validateOnBlur={false}
                 validate={validate}
-                render = {(props) => <MyDepositForm {...props} /> } />
-            );
+                render={(props) => <MyDepositForm {...props} />}/>
+        );
     }
 
     const Alert = (props) => {
         let c = success ? 'success' : 'danger';
-        return (<>{ message  && <div role="alert" className={`fade alert alert-${c} show`}>{message}</div> } </>) ;
+        return (<>{message && <div role="alert" className={`fade alert alert-${c} show`}>{message}</div>} </>);
 
     };
 
     return (
-         <React.Fragment>
-           <Header/>
-           <div className="by amt">
-             <div className="gc">
-                <SideBar loadCompetitions />
-                <div className="gz home">
-                    <div className="homepage">
-                      <FormTitle />
-                        <div className="col-md-12 mt-2 text-white p-2">
-                            <Alert />
-                            <div className="modal-body pb-0" data-backdrop="static">
-                                 <DepositForm />
+        <React.Fragment>
+            <Header/>
+            <div className="by amt">
+                <div className="gc">
+                    <SideBar loadCompetitions/>
+                    <div className="gz home">
+                        <div className="homepage">
+                            <FormTitle/>
+                            <div className="col-md-12 mt-2 text-white p-2">
+                                <Alert/>
+                                <div className="modal-body pb-0" data-backdrop="static">
+                                    <DepositForm/>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <Right/>
                 </div>
-                <Right/>
-             </div>
-           </div>
-           <Footer/>
-       </React.Fragment>
+            </div>
+            <Footer/>
+        </React.Fragment>
     )
 
 }
