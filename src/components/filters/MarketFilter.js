091a19961e1b5f7d1bc7d2e168ff302bb32b5379
Kelvin <kelvin@Kelvins-MacBook-Pro.local>
2022-07-15 20:28:38 +0300

    More markets

--- src/components/filters/MarketFilter.js
+++ src/components/filters/MarketFilter.js
@@ -9,10 +9,10 @@ const MarketFilter = () => {
         setShowFilters(false)
     }
     const markets = [
-        {id: "1", name: "1X2 Winner"},
-        {id: "10", name: "Double Chance"},
-        {id: "29", name: "Both Teams to Score"},
-        {id: "60", name: "First Half 1X2"},
+        {id: "1,18", name: "1X2 & Total Over/Under 2.5"},
+        {id: "1,10", name: "1X2 & Double Chance"},
+        {id: "1,29", name: "1X2 & Both Teams to Score"},
+        {id: "1,60", name: "1X2 & First Half 1X2"},
     ]
 
     const filterMarkets = (marketId) => {
