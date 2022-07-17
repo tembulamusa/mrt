1c901b769c528bf20d7fadcf83dcb4b8752a97e8
Kelvin <kelvin@Kelvins-MacBook-Pro.local>
2022-07-17 15:20:41 +0300

    Removing placeholder images

--- src/components/pages/promotions/Promotions.js
+++ src/components/pages/promotions/Promotions.js
@@ -21,19 +21,17 @@ const Promotions = ()=>{
                                 <div className="col">
                                     <div className={'row text-white p-2 shadow-sm'}>
                                         <div className="col-md-12 d-flex flex-row shadow-lg">
-                                            <div className="col-md-4">
-                                                Placeholder image
-                                            </div>
-                                            <div className="col-md-8">
+                                   
+                                            <div className="col-md-12">
                                                 <div className="d-flex flex-column">
                                                     <h5>20% FIRST DEPOSIT BONUS (DEPOSIT UPEWE)</h5>
                                                     <span><u>How to Play</u></span>
                                                     <ul>
                                                         <li>
-                                                            Deposit KES.50 to 500 get 20% as bonus
+                                                            ✅ Deposit KES.50 to 500 get 20% as bonus
                                                         </li>
                                                         <li>
-                                                            Deposit 500 and above get ksh.100 free (bonus). i.e. it is capped at Ksh.500. Any
+                                                            ✅ Deposit 500 and above get ksh.100 free (bonus). i.e. it is capped at Ksh.500. Any
                                                             amount above 500 bonus issued will be Ksh.100.
                                                         </li>
                                                     </ul>
@@ -41,18 +39,18 @@ const Promotions = ()=>{
                                                         <span><u>Terms and conditions</u></span>
                                                         <ol>
                                                             <li>
-                                                                Promotion is capped at KES.500 max
+                                                                ✅ Promotion is capped at KES.500 max
                                                             </li>
                                                             <li>
-                                                                Any deposit amounts exceeding 500 will only get KES.100
+                                                                ✅ Any deposit amounts exceeding 500 will only get KES.100
                                                             </li>
                                                             <li>
-                                                                The bonus is only to be issued ONCE, per player per day.
+                                                                ✅ The bonus is only to be issued ONCE, per player per day.
                                                             </li>
                                                             <li>
-                                                                The bonus is ONLY to  be issued on one transaction of KES 50 and above.
+                                                                ✅ The bonus is ONLY to  be issued on one transaction of KES 50 and above.
                                                             </li>
-                                                            <li>
+                                                            <li className={'p-2'}>
                                                                 Bonus rules apply
                                                             </li>
                                                         </ol>
@@ -61,10 +59,7 @@ const Promotions = ()=>{
                                             </div>
                                         </div>
                                         <div className="col-md-12 d-flex flex-row shadow-lg mt-2">
-                                            <div className="col-md-4">
-                                                Placeholder image
-                                            </div>
-                                            <div className="col-md-8">
+                                            <div className="col-md-12">
                                                 <div className="d-flex flex-column">
                                                     <h5>DAILY CASHBACK</h5>
                                                     <span><u>How to Play</u></span>
@@ -79,38 +74,38 @@ const Promotions = ()=>{
                                                         <span><u>Terms and conditions</u></span>
                                                         <ol>
                                                              <li>
-                                                                 This promotion will apply to your third (2.5 and above odds) cash bet made
+                                                                 ✅ This promotion will apply to your third (2.5 and above odds) cash bet made
                                                                  every day.
                                                              </li>
                                                             <li>
-                                                                The award in is CASH, which is credited to the customer’s wallet
+                                                                ✅ The award in is CASH, which is credited to the customer’s wallet
                                                             </li>
                                                             <li>
-                                                                The maximum amount issued as a cash back bonus will be KSH 100.
+                                                                ✅ The maximum amount issued as a cash back bonus will be KSH 100.
                                                             </li>
                                                             <li>
-                                                                Bets with bonus amounts will not be considered for this promotion as they
+                                                                ✅ Bets with bonus amounts will not be considered for this promotion as they
                                                                 are not cash bets.
                                                             </li>
                                                             <li>
-                                                                Jackpot bets will not be considered for this promotion.
+                                                                ✅ Jackpot bets will not be considered for this promotion.
                                                             </li>
                                                             <li>
-                                                                Voided Bets will be considered as cash refunds.
+                                                                ✅ Voided Bets will be considered as cash refunds.
                                                             </li>
                                                             <li>
-                                                                The promotion runs on Monday to Thursday ONLY and WILL NOT be
+                                                                ✅ The promotion runs on Monday to Thursday ONLY and WILL NOT be
                                                                 available on FRIDAY, SATURDAY & SUNDAY
                                                             </li>
                                                             <li>
-                                                                Betnare reserves the right to end the promotion at any time without any
+                                                                ✅  Betnare reserves the right to end the promotion at any time without any
                                                                 explanation or notification.
                                                             </li>
                                                             <li>
-                                                                Betnare reserves the right to disqualify any player at any time without being
+                                                                ✅ Betnare reserves the right to disqualify any player at any time without being
                                                                 bound to give reasons.
                                                             </li>
-                                                            <li>
+                                                            <li className={'p-2'}>
                                                                 Bonus rules apply
                                                             </li>
                                                         </ol>
@@ -119,10 +114,8 @@ const Promotions = ()=>{
                                             </div>
                                         </div>
                                         <div className="col-md-12 d-flex flex-row shadow-lg mt-2">
-                                            <div className="col-md-4">
-                                                Placeholder image
-                                            </div>
-                                            <div className="col-md-8">
+                                   
+                                            <div className="col-md-12">
                                                 <div className="d-flex flex-column">
                                                     <h5>MULTIBET LOST BONUS</h5>
                                                     <span><u>How to Play</u></span>
@@ -131,7 +124,7 @@ const Promotions = ()=>{
                                                             Place a pre-match Multibet / accumulator of 5 or more selections.
                                                         </li>
                                                         <li>
-                                                            If your bet is unsuccessful from 1 loss out of the accumulator, we will refund a
+                                                            ✅ If your bet is unsuccessful from 1 loss out of the accumulator, we will refund a
                                                             bonus of a set percentage based on the matrix set on your stake. i.e., The bonus
                                                             amount is dependent on the number of selections
                                                         </li>
@@ -140,31 +133,31 @@ const Promotions = ()=>{
                                                         <span><u>Terms and conditions</u></span>
                                                         <ol>
                                                              <li>
-                                                                 This offer ONLY applies to returns on pre-match accumulators
+                                                                 ✅ This offer ONLY applies to returns on pre-match accumulators
                                                              </li>
                                                             <li>
-                                                                The maximum bonus that you can receive is KES.500
+                                                                ✅ The maximum bonus that you can receive is KES.500
                                                             </li>
                                                             <li>
-                                                                Betnare may reclaim any bonus amount, Free Bets, shinda points or enhanced
+                                                                ✅ Betnare may reclaim any bonus amount, Free Bets, shinda points or enhanced
                                                                 payments that have been awarded in error.
                                                             </li>
                                                             <li>
-                                                                Betnare may, at any time, make minor amendments to this promotion to correct
+                                                                ✅ Betnare may, at any time, make minor amendments to this promotion to correct
                                                                 typographical errors or to improve on clarity or customer experience and may
                                                                 cancel this promotion for legal or regulatory reasons.
                                                             </li>
                                                             <li>
-                                                                Bets placed with bonuses do not apply/qualify for this offer
+                                                                ✅ Bets placed with bonuses do not apply/qualify for this offer
                                                             </li>
                                                             <li>
-                                                                The refund is paid in BONUS and bonus rules apply
+                                                                ✅ The refund is paid in BONUS and bonus rules apply
                                                             </li>
                                                             <li>
-                                                                Jackpot bets will not apply/ be considered for this promotion
+                                                                ✅ Jackpot bets will not apply/ be considered for this promotion
                                                             </li>
                                                             <li>
-                                                                Voided Bets will not apply/ be considered.
+                                                                ✅ Voided Bets will not apply/ be considered.
                                                             </li>
                                                         </ol>
                                                     </div>
