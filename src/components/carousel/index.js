7be606556fb2ffeed814655141a446d46d7a802c
Kelvin <kelvin@Kelvins-MacBook-Pro.local>
2022-07-20 10:42:38 +0300

    Added homepage sliders

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -5,17 +5,19 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 
 // import banner1  from '../../assets/img/banner/banner001.jpeg';
 
-import banner1 from '../../assets/img/banner/homepage/KARIBU.png';
-import banner6 from '../../assets/img/banner/homepage/REGISTER.png';
-import banner2 from '../../assets/img/banner/homepage/50Bob.png';
-import banner3 from '../../assets/img/banner/homepage/CASHBACK.png';
-import banner4 from '../../assets/img/banner/homepage/DEPOSIT BONUS.png';
-import banner5 from '../../assets//img/banner/homepage/JACKPOT.png';
-import banner7 from '../../assets/img/banner/homepage/MASTER.png';
-import banner8 from '../../assets/img/banner/homepage/ODDS.png';
+
+// import banner6 from '../../assets/img/banner/homepage/REGISTER.png';
+// import banner3 from '../../assets/img/banner/homepage/CASHBACK.png';
+// import banner4 from '../../assets/img/banner/homepage/DEPOSIT BONUS.png';
+
+import banner2 from '../../assets/img/banner/homepage/virtuals.jpg';
+import banner1 from '../../assets/img/banner/homepage/karibu.png';
+import banner5 from '../../assets/img/banner/homepage/FREEBET.png';
+import banner7 from '../../assets/img/banner/homepage/ChezaLive.png';
+import banner8 from '../../assets/img/banner/homepage/BEST ODDS.png';
 
 const banners = [
-    banner1, banner2, banner3, banner4, banner5, banner6, banner7, banner8
+    banner1, banner2, banner5, banner7, banner8
 ]
 
 const CarouselLoader = (props) => {
