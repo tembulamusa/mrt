1c12601262c726c6c693386b0962a97b18fc4676
TychaK <kelvinkibugi@gmail.com>
2022-07-29 11:09:18 +0300

    Updated carousel images

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -3,14 +3,14 @@ import Carousel from 'react-bootstrap/Carousel';
 import {LazyLoadImage} from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
-import banner2 from '../../assets/img/banner/homepage/virtuals.jpg';
-import banner1 from '../../assets/img/banner/homepage/karibu.png';
-import banner5 from '../../assets/img/banner/homepage/FREEBET.png';
-import banner7 from '../../assets/img/banner/homepage/ChezaLive.png';
-import banner8 from '../../assets/img/banner/homepage/BEST ODDS.png';
+import banner4 from '../../assets/img/banner/products/20PercentFirstdepositBonus-min.png'
+import banner2 from '../../assets/img/banner/products/30PercentDAILYCASHBACK.png'
+import banner3 from '../../assets/img/banner/products/70PercentMULTIBETCASHBACK.png'
+import banner1 from '../../assets/img/banner/products/KaribuBonus.png'
+import banner5 from '../../assets/img/banner/products/100PercentDepositBonus.png'
 
 const banners = [
-    banner1, banner2, banner5, banner7, banner8
+    banner1, banner2, banner3, banner4, banner5
 ]
 
 const CarouselLoader = (props) => {
