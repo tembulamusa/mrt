34c536d233742d4db4a9e1c9740d75439e5d6028
root <root@bn-dmk-web-1.europe-west1-b.c.betnare.internal>
2022-08-05 10:48:33 +0300

    Merged

--- src/components/jackpot.js
+++ src/components/jackpot.js
@@ -53,11 +53,6 @@ const Jackpot = (props) => {
                                 id=""
                                 className="background-primary "
                                 justify>
-<<<<<<< HEAD
-                                <Tab eventKey="home" title="Jackpot" className={'background-primary '}>
-                                    <JackpotHeader jackpot={matches?.meta}/>
-                                    <JackpotMatchList matches={matches}/>
-=======
                                 <Tab eventKey="home" title="Jackpot" className={'background-primary'}>
                                     {matches?.length > 0 ? (
                                         <>
@@ -69,7 +64,6 @@ const Jackpot = (props) => {
                                             There are no active jackpots at the moment.
                                         </div>
                                     )}
->>>>>>> origin/ch-markets
                                 </Tab>
                                 <Tab eventKey="results" title="Results" >
                                     Jackpot results will be shown here...
