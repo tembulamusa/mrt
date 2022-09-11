0731b5013c86085ec99c17060df241705ac9d5ea
Reuben Wafula <rubewafula@gmail.com>
2022-09-12 02:29:35 +0300

    FIX : adding image and stuff

--- src/components/right/company-info.js
+++ src/components/right/company-info.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import RightBanner from '../../assets/img/right-banner.jpg'
+import RightBanner from '../../assets/img/Virtual-side-Banner.png'
+import PayBill from '../../assets/img/PayBill.png'
 
 const CompanyInfo = (props) => {
 
@@ -8,8 +9,8 @@ const CompanyInfo = (props) => {
             <div className="qv rc alu paybill block-shadow bottom-std-margin-spacing" style={{margin:"5px"}}>
                 <div className="">
                     <div className="">
-                        <span className="col-sm-4">
-                        {/*<img src={contact} alt=" " />*/}
+                        <span className="col-sm-4 h-50">
+                         <img src={PayBill} alt=" " style={{width:"100%", marginBottom:"5px"}}/>
                         </span>
                         <span className="col-sm-8">
                             <img src={RightBanner} alt="" className='w-100'/>
