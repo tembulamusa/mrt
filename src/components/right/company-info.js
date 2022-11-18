d422687edc39e8bdab68f0f21bc60d77c7a301ad
Reuben Wafula <rubewafula@gmail.com>
2022-11-18 17:07:47 +0300

    FIX: Add customer support line

--- src/components/right/company-info.js
+++ src/components/right/company-info.js
@@ -9,12 +9,24 @@ const CompanyInfo = (props) => {
             <div className="qv rc alu paybill block-shadow bottom-std-margin-spacing" style={{margin:"5px"}}>
                 <div className="">
                     <div className="">
-                        <span className="col-sm-4 h-50">
-                         <img src={PayBill} alt=" " style={{width:"100%", marginBottom:"5px"}}/>
-                        </span>
-                        <span className="col-sm-8">
+                        <div className="col-sm-12 h-30">
+                         <img src={PayBill} alt=" " style={{width:"100%"}}/>
+                        </div>
+                        <div className="col-sm-12 h-30 " style={{
+                            lineHeight:"20px",
+                            background: "#613354",
+                            margin: 0, 
+                            padding: "10px 5px", 
+                            color: "#fff", 
+                            fontWeight: "bold",
+                            fontSize:"24px"
+                        }}>
+                          <p>Customer Care</p>
+                          <p>0742800700 </p>
+                        </div>
+                        <div className="col-sm-12">
                             <img src={RightBanner} alt="" className='w-100'/>
-                        </span>
+                        </div>
                     </div>
                 </div>
             </div>
