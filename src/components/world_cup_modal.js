08ba846ec93e78aa06961c18a35cfaf7ab8ff0b5
Tembla Moses <woesites@gmail.com>
2022-11-30 16:56:27 +0300

    FIX: fix unfound image

--- src/components/world_cup_modal.js
+++ src/components/world_cup_modal.js
@@ -15,23 +15,13 @@ const WorldCupModal = (props) => {
             onHide={() => setShow(false)}
             dialogClassName="modal-90w world-cup-ad"
             aria-labelledby="contained-modal-title-vcenter">
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter" >
-                        <h3 className="text-center">WIN BIG WITH WORLD CUP BETS</h3>
-                    </Modal.Title>
-                </Modal.Header>
-
                 <a href="/competition/79/8085/18585">
                     <Modal.Body>
                         <LazyLoadImage
                             className="d-block w-100"
                             src={worldcup}
                             alt="bethipo" />
-                        <Row className="w-100 justify-content-center text-center">
-                            <Col>
-                                <h4>CLICK HERE TO BET ON WORLD CUP AND WIN BIG</h4>
-                            </Col>
-                        </Row>
+                        
                     </Modal.Body>
                 </a>
             </Modal>
