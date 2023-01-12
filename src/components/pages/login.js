c2635d9477173ed0a4da0815881a38abca316f6b
Tembla Moses <woesites@gmail.com>
2023-01-13 00:32:04 +0300

    register

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -4,6 +4,7 @@ import makeRequest from "../utils/fetch-request";
 import mpesa from '../../assets/img/mpesa-3.png'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import { useLocation } from 'react-router-dom';
 
 const Header = React.lazy(() => import('../header/header'));
 const SideBar = React.lazy(() => import('../sidebar/awesome/Sidebar'));
@@ -13,10 +14,11 @@ const BodyLogin = React.lazy(() => import('../header/mobile-login'));
 
 
 const Login = (props) => {
+    // const {state} = useLocation();
+    // const {regMessage} = state;
     const [message, setMessage] = useState(null);
     const [success, setSuccess] = useState(false);
     const [loading, setLoading] = useState(false);
-
 
     
     const FormTitle = () => {
@@ -48,6 +50,7 @@ const Login = (props) => {
                             <div className="row">
                             <div className="col-md-12 mt-2  p-2">
                                 {message && <Alert/>}
+
                                 <div className="modal-body pb-0" data-backdrop="static">
                                     <BodyLogin/>
                                 </div>
