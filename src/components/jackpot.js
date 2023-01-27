4a2efd188cfe799a83ef3f253e6f9fbb71b4adc9
Tembla Moses <woesites@gmail.com>
2023-01-27 12:38:32 +0300

    add jackpot

--- src/components/jackpot.js
+++ src/components/jackpot.js
@@ -14,19 +14,50 @@ const Right = React.lazy(() => import('./right/index'));
 
 const Jackpot = (props) => {
     const [matches, setMatches] = useState(null);
+    const [weeklyJPMatches, setWeeklyJPMatches] = useState(null);
+    const [dailyJPMatches, setDailyJPMatches] = useState(null);
+    const [message, setMessage] = useState(null);
+    const [showEmptyWeeklyJackpot, setShowEmptyWeeklyJackpot] = useState(false);
+    const [showEmptyDailyJackpot, setShowEmptyDailyJackpot] = useState(false);
+    const [disabledWeekly, setDisabledWeekly] = useState(false);
+    const [disabledDaily, setDisabledDaily] = useState(false);
+    const [currentJackpot, setCurrentJackpot] = useState(null);
 
-    const fetchData = useCallback(async () => {
-        let match_endpoint = "/v1/matches/jackpot";
+    const fetchData = (jpType) => {
+        let match_endpoint = `/v1/matches/jackpot?key=${jpType}`;
 
-        const [match_result] = await Promise.all([
-            makeRequest({url: match_endpoint, method: "get", data: null})
-        ]);
-        let [m_status, m_result] = match_result;
-        if (m_status === 200) {
-            setMatches(m_result);
-        }
+        makeRequest({url: match_endpoint, method: "get", data: null}).then(
+            ([m_status, m_result]) => {
+                if (m_status === 200) {
 
-    }, []);
+                if (jpType == "jp"){
+                    setDailyJPMatches(m_result);
+                    setShowEmptyDailyJackpot(true);
+                    setDisabledDaily(true);
+                    setWeeklyJPMatches(false);
+                    setShowEmptyWeeklyJackpot(false)
+                    setDisabledWeekly(false);
+                    setCurrentJackpot(dailyJPMatches);
+                } else if (jpType == "wjp") {
+                    setWeeklyJPMatches(m_result);
+                    setShowEmptyWeeklyJackpot(true);
+                    setDisabledWeekly(true);
+                    setDailyJPMatches(false);
+                    setShowEmptyDailyJackpot(false)
+                    setDisabledDaily(false);
+                    setCurrentJackpot(weeklyJPMatches);
+                } else {
+                    setMatches(m_result);
+                }
+
+                
+            } else {
+                setMessage("An error occurred");
+            }
+            });
+        
+
+    };
 
 
     const EmptyJackpotPageMarkup = (props) => {
@@ -36,56 +67,116 @@ const Jackpot = (props) => {
             </div>
             )
     }
+
+    const JackpotsHeader = () => {
+        return (
+            <div className="jp-header">
+                <div className="row">
+                    <div className="col-2"></div>
+                    <div className="col-3"></div>
+                    <div className="col">
+                        <span className="jp-pick-type">Home</span> 
+                        <span className="jp-pick-type">Draw</span>
+                        <span className="jp-pick-type">Away</span>
+                    </div>
+                </div>
+            </div>
+            )
+    }
     const JackpotsListing = () => {
         return (
             <div className="jackpots">
-                <div className="weekly-jackpot jackpot-entry p-3 pb-4 mt-3 mb-3 primary-bg">
-                    <div className="row">
-                        {/* We'll fetch the jackpots first. due to detail */}
-                        <div className="col-4">
-                            <h3>TSH. 50,000,000</h3>
-                        </div>
+                <div className="jackpot-body">
+                    <div className="weekly-jackpot jackpot-entry p-3 pb-4 mt-3 mb-3 primary-bg">
+                        <div className="row">
+                            {/* We'll fetch the jackpots first. due to detail */}
+                            <div className="col-4">
+                                <h3>TSH. 50,000,000</h3>
+                            </div>
 
-                        <div className="col-4">
-                            <h4 className="uppercase">Biko weekly jackpot</h4>
-                            <p className="more-info-text detail-text">Pick 17 games, Prizes from correct 13</p>
-                        </div>
+                            <div className="col-4">
+                                <h4 className="uppercase">Biko weekly jackpot</h4>
+                                <p className="more-info-text detail-text">Pick 17 games, Prizes from correct 13</p>
+                            </div>
 
-                        <div className="col-4">
-                            <h4>30/01/2023 4.00 PM</h4>
-                        </div>
+                            <div className="col-4">
+                                <h4>30/01/2023 4.00 PM</h4>
+                            </div>
 
-                        <div className="col-12">
-                            <button className="bold mt-2 btn-lg btn btn-primary deposit-withdraw-button secondary-red-bg uppercase btn-play-jackpot btn-weekly full-width">
-                                Play for TSH. 5
-                            </button>
+                            <div className="col-12">
+                                <button onClick={() => fetchData("wjp")}
+                                    disabled={disabledWeekly}
+                                    className="bold mt-2 btn-lg btn btn-primary deposit-withdraw-button secondary-red-bg uppercase btn-play-jackpot btn-weekly full-width">
+                                    Play for TSH. 5
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="jackpot-matches">
+                        <div className="weekly-jp jackpot-games">
+
+                            {weeklyJPMatches?.data?.length > 0 ? (
+                                <>  
+                                    <JackpotsHeader />
+                                    <JackpotMatchList matches={weeklyJPMatches}/>
+                                </>
+                            ) : ( showEmptyWeeklyJackpot? (
+                                    <div className={'col-md-12 text-center background-primary  mt-2 p-3'}>
+                                        There are no active Weekly jackpots at the moment.
+                                    </div>) : ("") 
+                                
+                            )}
                         </div>
                     </div>
                 </div>
+                <div className="jackpot-body">
+                    <div className="daily-jackpot jackpot-entry p-3 pb-4 mt-4 yellow-bg-2">
+                        <div className="row">
+                            {/* We'll fetch the jackpots first. due to detail */}
+                            <div className="col-4">
+                                <h3 className="black-text-shadow">TSH. 10,000,000</h3>
+                            </div>
 
-                <div className="daily-jackpot jackpot-entry p-3 pb-4 mt-4 yellow-bg-2">
-                    <div className="row">
-                        {/* We'll fetch the jackpots first. due to detail */}
-                        <div className="col-4">
-                            <h3 className="black-text-shadow">TSH. 10,000,000</h3>
-                        </div>
+                            <div className="col-4">
+                                <h4 className="uppercase black-text-shadow">Biko Daily jackpot</h4>
+                                <p className="bold more-info-text detail-text">Pick 13 games, Prizes from correct 8</p>
+                            </div>
 
-                        <div className="col-4">
-                            <h4 className="uppercase black-text-shadow">Biko Daily jackpot</h4>
-                            <p className="bold more-info-text detail-text">Pick 13 games, Prizes from correct 8</p>
-                        </div>
+                            <div className="col-4 black-text-shadow">
+                                <h4>29/01/2023  4.00PM</h4>
+                            </div>
 
-                        <div className="col-4 black-text-shadow">
-                            <h4>29/01/2023  4.00PM</h4>
-                        </div>
+                            <div className="col-12">
 
-                        <div className="col-12">
-                            <button className=" mt-2 btn-lg btn btn-primary bold deposit-withdraw-button uppercase btn-play-jackpot btn-daily full-width">
-                                Play for TSH. 5
-                            </button>
+                                <button onClick={() => fetchData("jp")}
+                                    disabled={disabledDaily}
+                                    className="mt-2 btn-lg btn btn-primary bold deposit-withdraw-button uppercase btn-play-jackpot btn-daily full-width">
+                                    Play for TSH. 5
+                                </button>
+                            </div>
                         </div>
                     </div>
+
+                    <div className="jackpot-matches">
+                        <div className="daily-jp jackpot-games">
+
+                            {dailyJPMatches?.data?.length > 0 ? (
+                                <>  
+                                    <JackpotsHeader />
+                                    <JackpotMatchList matches={dailyJPMatches}/>
+                                </>
+                            ) : ( showEmptyWeeklyJackpot? (
+                                    <div className={'col-md-12 text-center background-primary  mt-2 p-3'}>
+                                        There are no active Weekly jackpots at the moment.
+                                    </div>) : ("") 
+                                
+                            )}
+                        </div>
+                    </div>
+
                 </div>
+
+
             </div>
         )
     }
@@ -259,15 +350,7 @@ const Jackpot = (props) => {
         )
     }
 
-    useEffect(() => {
-
-        const abortController = new AbortController();
-        //fetchData();
-
-        return () => {
-            abortController.abort();
-        };
-    }, [fetchData]);
+    
 
     return (
         <>
@@ -285,7 +368,7 @@ const Jackpot = (props) => {
                             }
                         </div>
                     </div>
-                    <Right jackpot={true} jackpotData={matches?.meta}/>
+                    <Right jackpot={true} jackpotData={currentJackpot?.meta}/>
                 </div>
             </div>
             <Footer/>
