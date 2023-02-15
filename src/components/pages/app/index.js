73542e2d0f845f949e83ccf949415be0a653ab16
Tembla Moses <woesites@gmail.com>
2023-02-15 18:23:18 +0300

    Style inner pages

--- src/components/pages/app/index.js
+++ src/components/pages/app/index.js
@@ -28,34 +28,24 @@ const MobileApp = () => {
                     <div className="gz home" style={{width: '100%'}}>
                         <div className="homepage">
                             <div
-                                className='col-md-12 primary-bg p-4 text-center d-flex flex-row justify-content-between sticky-top'>
+                                className='col-md-12 primary-bg p-2 text-center d-flex flex-row justify-content-between sticky-top'>
                                 <h4 className="inline-block">
                                     Bikosports APP
                                 </h4>
-                                <DownloadLink
-                                    className="btn btn-primary bold btn-danger btn-lg text-white text-decoration-none" 
-                                    style={{width: "130px", height: "35px"}}
-                                    label="Download App"
-                                    filename="bikosports.apk"
-                                    exportFile={() => getDownloadFile()}
-                                />
-                                
                             </div>
                             <div className="col-md-12 mt-2 accordion-container text-start pad-hor-10 pad-vert-10">
                                 <Row>
                                     <div className="col-sm-12 col-md-6">
-                                    <p>For the best experience with android phone download bikosports app from the link below</p>
+                                    <p className="uppercase bold center-text">bikosports app provides better experience on mobile</p>
                                     <DownloadLink
-                                        className="btn btn-primary bold btn-danger btn-lg text-white text-decoration-none"
+                                        className="full-width bold btn-danger btn-lg text-white text-decoration-none btn btn-secondary btn-download secondary-red-bg btn-lg p-4 dd"
                                         label="Download App"
                                         filename="bikosports.apk"
                                         exportFile={() => getDownloadFile()}
                                     />
-                                    <p>See picture next for details on how android app works.</p>
                                     </div>
 
                                     <div className="col-sm-12 col-md-6">
-                                        <img src={mobileBanner} alt="mobile download"/>
                                     </div>
                                 </Row>
                             </div>
