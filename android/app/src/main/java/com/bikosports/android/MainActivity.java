060ff030de5980bb6846c7e35ce06b3c2b1b764a
Hanningtone Ommila <hansmiller002@gmail.com>
2023-02-28 14:54:26 +0300

    Working on Versioning shit

--- android/app/src/main/java/com/bikosports/android/MainActivity.java
+++ android/app/src/main/java/com/bikosports/android/MainActivity.java
@@ -1,5 +1,8 @@
 package com.bikosports.android;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
@@ -11,4 +14,14 @@ public class MainActivity extends BridgeActivity {
         registerPlugin(EchoPlugin.class);
         super.onCreate(savedInstanceState);
     }
+    public static double getVersionCode(Context ctx) {
+        try {
+            PackageManager manager = ctx.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(ctx.getPackageName(), 0);
+            return Double.parseDouble("" + info.versionCode);
+        } catch (PackageManager.NameNotFoundException e) {
+            return -1.0;
+        }
+    }
+
 }
