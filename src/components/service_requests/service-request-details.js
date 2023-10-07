480746a0c9eb32465976f92474fbf757c9e331cd
moses tembula <jameswangabivan@gmail.com>
2023-10-07 17:26:03 +0300

    add service car and airline

--- src/components/service_requests/service-request-details.js
+++ src/components/service_requests/service-request-details.js
@@ -104,13 +104,97 @@ const ServiceRequestDetail = (props) => {
         } else if (selectedService.includes("airline")) {
             return (
                 <>
-                    Airline return
+                    <table className="w-full">
+                    <tbody className="[&>*:nth-child(odd)]:bg-blue-50">
+                        <tr className="w-full">
+                            <td className="border border-gray-100 p-2">Status</td>
+                            <td className="border border-gray-100 p-2">selectable status</td>
+                        </tr>
+                        <tr className="w-full">
+                            <td className="border border-gray-100 p-2">Date Created</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.createdAt}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-100 p-2">Location</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.location}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-100 p-2">Location Type</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.locationType}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-100 p-2">Journey Type</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.journeyType}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-100 p-2">Origin</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.origin}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-100 p-2">Destination</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.destination}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-100 p-2">Ticket Category</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.ticketCategory}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-100 p-2">Number of Tickets</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.ticketCount}</td>
+                        </tr>
+                        
+                        <tr>
+                            <td className="border border-gray-100 p-2">Departure Date</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.departureDate}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-100 p-2">Return Date</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.returnDate}</td>
+                        </tr>
+                    </tbody>
+                </table>
                 </>
             )
         }else if (selectedService.includes("car")) {
             return (
                 <>
-                    Car hire returned
+                    <table className="w-full">
+                    <tbody className="[&>*:nth-child(odd)]:bg-blue-50">
+                        <tr className="w-full">
+                            <td className="border border-gray-100 p-2">Status</td>
+                            <td className="border border-gray-100 p-2">selectable status</td>
+                        </tr>
+                        <tr className="w-full">
+                            <td className="border border-gray-100 p-2">Date Created</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.createdAt}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-100 p-2">Location</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.location}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-100 p-2">Location Type</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.locationType}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-100 p-2">Car Type</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.carType}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-100 p-2">Number of Cars</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.carCount}</td>
+                        </tr>
+                        
+                        <tr>
+                            <td className="border border-gray-100 p-2">Starting Date</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.fromDate}</td>
+                        </tr>
+                        <tr>
+                            <td className="border border-gray-100 p-2">End Date</td>
+                            <td className="border border-gray-100 p-2">{state?.latestmemoserviceitem?.toDate}</td>
+                        </tr>
+                    </tbody>
+                </table>
                 </>
             )
         }else {
