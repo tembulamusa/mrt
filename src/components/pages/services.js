566b3e894b88b0b29ce4a7776f07ddad9f1ba9ab
Tembla Moses <woesites@gmail.com>
2024-02-07 01:06:19 +0300

    FEAT: add images on posts

--- src/components/pages/services.js
+++ src/components/pages/services.js
@@ -1,5 +1,12 @@
 import React from "react";
 import PageHeader from "../utils/page-header";
+import Domestic from "../../assets/img/services/packing.jpg"
+import Conso from "../../assets/img/services/conso.jpg"
+import Machine from "../../assets/img/services/machine.png"
+import Warehouse from "../../assets/img/services/warehouse.jpg"
+import Packing from "../../assets/img/services/packing.jpg"
+import Office from "../../assets/img/services/house.jpg"
+
 const Services = (props) => {
 
     return (
@@ -10,6 +17,7 @@ const Services = (props) => {
                 <div className="container flex">
                     <div className="float-end w-1/3 inline-block my-2 p-2">
                         <div className="p-2 text-center block border border-gray-200 rounded">
+                            <img src={Domestic} className="w-20 rounded-full mx-auto my-2"/>
                             <h1 className="text-2xl font-bold leading-8 py-3">Domestic & Inter-County House Moving</h1>
                             <div className="leading-6 pb-4">
                                 Whether you are moving across the street or to a new county, our team handles everything from packing to final arrangement at your new house.
@@ -19,6 +27,7 @@ const Services = (props) => {
 
                     <div className="float-end w-1/3 inline-block my-2 p-2">
                         <div className="p-2 text-center block border border-gray-200 rounded">
+                            <img src={Office} className="w-20 rounded-full mx-auto my-2"/>
                             <h1 className="text-2xl font-bold leading-8 py-3">Office Moving and Relocation</h1>
                             <div className="leading-6 pb-4">
                                 We are especially adept in moving large organizations. We ensure all assets handled with maximum safety. Enjoy the best corporate moves
@@ -27,6 +36,7 @@ const Services = (props) => {
                     </div>
                     <div className="float-end w-1/3 inline-block my-2 p-2">
                         <div className="p-2 text-center block border border-gray-200 rounded">
+                            <img src={Warehouse} className="w-20 rounded-full mx-auto my-2"/>
                             <h1 className="text-2xl font-bold leading-8 py-3">Warehousing and Storage Services</h1>
                             <div className="leading-6 pb-4">
                                 Store you household and commercial goods in our warehouse at 15% off to celebrate 15 years of Mara Movers.
@@ -39,6 +49,7 @@ const Services = (props) => {
                 <div className="container flex">
                     <div className="float-end w-1/3 inline-block my-2 p-2">
                         <div className="p-2 text-center block border border-gray-200 rounded">
+                            <img src={Packing} className="w-20 rounded-full mx-auto my-2"/>
                             <h1 className="text-2xl font-bold leading-8 py-3">Packing & Crating Services</h1>
                             <div className="leading-6 pb-4">
                                 For all your international packing and crating needs, we customize crates from certified fumigated timber as per ISPM 15 standards.
@@ -48,6 +59,7 @@ const Services = (props) => {
 
                     <div className="float-end w-1/3 inline-block my-2 p-2">
                         <div className="p-2 text-center block border border-gray-200 rounded">
+                            <img src={Machine} className="w-20 rounded-full mx-auto my-2"/>
                             <h1 className="text-2xl font-bold leading-8 py-3">Machine Relocation</h1>
                             <div className="leading-6 pb-4">
                                 Do you require to move sophisticated equipment such as ATM machines, safes, bulk filers and much more.
@@ -56,6 +68,7 @@ const Services = (props) => {
                     </div>
                     <div className="float-end w-1/3 inline-block my-2 p-2">
                         <div className="p-2 text-center block border border-gray-200 rounded">
+                            <img src={Conso} className="w-20 rounded-full mx-auto my-2"/>
                             <h1 className="text-2xl font-bold leading-8 py-3">Pwani Conso</h1>
                             <div className="leading-6 pb-4">
                                 We offer this service for small household consignments between Nairobi and Mombasa and vice-versa.
