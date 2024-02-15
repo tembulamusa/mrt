d8cc90150942251e818b671a874199bf2e1af65a
Tembla Moses <woesites@gmail.com>
2024-02-15 14:49:44 +0300

    FEAT: customize our promise section for mobile

--- src/components/our-promise.js
+++ src/components/our-promise.js
@@ -11,10 +11,10 @@ const OurPromise = (props) => {
         <>
             <section className="py-4 mb-2">
                 <div className="container">
-                    <h1 className="text-blue-600 font-bold capitalize mb-4 text-center text-6xl ">Our Promise</h1>
+                    <h1 className="text-blue-600 font-bold capitalize mb-4 text-center text-4xl md:text-6xl ">Our Promise</h1>
 
-                    <div>
-                        <p>We know that moving is very personal experience that requires meticulous understanding of your needs. That’s why we developed The Mara Movers Way, a removals methodology built around this philosophy.</p>
+                    <div className="text-center md:text-left">
+                        <p className="mb-3">We know that moving is very personal experience that requires meticulous understanding of your needs. That’s why we developed The Mara Movers Way, a removals methodology built around this philosophy.</p>
 
                         <p>Every move starts with a detailed assessment of your requirements and the nature of goods at hand. This informs the way we pack and handle each category of goods to ensure their integrity is maintained. Experience the delight of having breakfast at your old house, and by evening, enjoying dinner with feet up at your new house, all without breaking a sweat! We also offer secure and affordable warehousing and storage solutions.</p>
                     </div>
@@ -22,7 +22,7 @@ const OurPromise = (props) => {
             </section>
             <section className='bg-white my-3 mb-4' id='search-section h-1.5'>
                 <div className='container relative w-full'>                    
-                    <div className='w-1/3 pr-4 mt-2 inline-block'>
+                    <div className='w-full md:w-1/3 pr-4 mt-2 inline-block'>
                         <div className="border border-gray-200 p-3 shadow-sm text-center">
                             <div className="text-center"><FaHandshake size={40} className="text-blue-600 mx-auto"/></div>
                             <div className="text-2xl capitalize my-3 font-bold">Trusted Movers in Kenya</div>
@@ -32,7 +32,7 @@ const OurPromise = (props) => {
                         </div>
                     </div>
 
-                    <div className='w-1/3 pr-4 mt-2 inline-block'>
+                    <div className='w-full md:w-1/3 pr-4 mt-2 inline-block'>
                         <div className="border border-gray-200 p-3 shadow-sm text-center">
                             <div className="text-center"><FaCommentDollar size={40} className="text-blue-600 mx-auto"/></div>
                             <div className="text-2xl capitalize my-3 font-bold">Professional Services</div>
@@ -42,7 +42,7 @@ const OurPromise = (props) => {
                         </div>
                     </div>
 
-                    <div className='w-1/3 pr-4 mt-2 inline-block'>
+                    <div className='w-full md:w-1/3 pr-4 mt-2 inline-block'>
                         <div className="border border-gray-200 p-3 shadow-sm text-center">
                             <div className="text-center"><FaRegMoneyBillAlt size={40} className="text-blue-600 mx-auto"/></div>
                             <div className="text-2xl capitalize my-3 font-bold">Affordable Storage</div>
