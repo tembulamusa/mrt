93cf88d331af58d70fa07f6a9867880781384ab7
Tembla Moses <woesites@gmail.com>
2024-02-15 14:40:05 +0300

    FEAT: customize jumbotron for mobile

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -9,7 +9,7 @@ const Jumbotron = (props) => {
                 <div className="w-full bg-transluscent py-3">
                     <div className='container py-7'>
                         <div className='w-full py-6 inline-block'>
-                            <div className='text-5xl py-3 font-bold leading-none'>Kenya’s Number 1 <br/>Movers <br/>Website</div>
+                            <div className='text-3xl md:text-5xl py-3 font-bold leading-9'>Kenya’s Number 1 <br />Movers <span className="d-none md:inline-block"><br/></span>Website</div>
                             <div className='py-2'>
                                 We offer exlussive services with regards to moving including:<br/>House Moving, Office, Corporate
                             </div>
